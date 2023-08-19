623574405e44890bd6045f6305520615195b915c
rohit <rohitchandan998@gmail.com>
2023-08-19 22:12:55 +0530

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className=" container">
         <div className="">
           <video autoPlay muted loop className="fixed top-0 left-0 -z-20 w-full max-h-[80vh] min-h-[60vh] object-cover">
-            <source src="/assests/shroom-video2.mp4" type="video/mp4" />
+            <source src="/assests/shroom-video4.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           <div className=" inset-0 bg-black/60 absolute -z-10 max-h-[80vh]"></div>
